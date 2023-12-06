30763df9d96028101f7935b3a5d5c43b232113b1
Adham <Adhammorsy75@gmail.com>
2023-12-06 18:48:05 -0500

    login

--- Front-End/src/components/Login.jsx
+++ Front-End/src/components/Login.jsx
@@ -34,10 +34,7 @@ const Login = ({ setIsAuthenticated }) => {
                     rememberMe: rememberMe,
                 }
             );
-            if (res.status === 200) {
-                // Extract the token from the response
-                const { token, user } = res.data;
-
+            
                 if (token) {
                     // Store the token in a secure cookie using document.cookie
                     document.cookie = `jwt=${token}; max-age=${86400}; path=/; samesite=strict`;
@@ -56,4 +53,89 @@ const Login = ({ setIsAuthenticated }) => {
                     navigate("/");
                 }
             }
-      
+        } catch (error) {
+        
+            else if (error.response.status === 401) {
+                window.alert('User Not Found');
+            }
+            else {
+                window.alert('Internal Server Error');
+            }
+        }
+    };
+
+    return (
+        <div>
+            <div className="container shadow my-5">
+                <div className="row">
+                    <div className="col-md-5 d-flex flex-column align-items-center text-white justify-content-center form">
+                        <h1 className="display-4 fw-bolder">Welcome Back</h1>
+                        <p className="lead text-center">Enter Your Credentials To Login</p>
+                        <h5 className="mb-4">OR</h5>
+                        <NavLink
+                            to="/register"
+                            className="btn btn-outline-light rounded-pill pb-2 w-50"
+                        >
+                            Register
+                        </NavLink>
+                    </div>
+                    <div className="col-md-6 p-5">
+                        <h1 className="display-6 fw-bolder mb-5">LOGIN</h1>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputEmail1" className="form-label">
+                                    Email address
+                                </label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="exampleInputEmail1"
+                                    aria-describedby="emailHelp"
+                                    name="email"
+                                    value={user.email}
+                                    onChange={handleChange}
+                                />
+                                <div id="emailHelp" className="form-text">
+                                    We'll never share your email with anyone else.
+                                </div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputPassword1" className="form-label">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="exampleInputPassword1"
+                                    name="password"
+                                    value={user.password}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                           
+                                <input
+                                    type="checkbox"
+                                    className="form-check-input"
+                                    id="rememberMeCheckbox"
+                                    checked={rememberMe}
+                                    onChange={() => setRememberMe(!rememberMe)}
+                                />
+                                <label className="form-check-label" htmlFor="rememberMeCheckbox">
+                                    Remember me
+                                </label>
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary w-100 mt-4 rounded-pill"
+                            >
+                                Login
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
