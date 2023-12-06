8758ce7d284aa18d7daafc633e3a076243f9072a
Adham <Adhammorsy75@gmail.com>
2023-12-06 14:37:10 -0500

    full-modules

---
+++ Back-End/Models/userSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+})
+
+//Create Model
+const Users = new mongoose.model("Users", userSchema);
+
+module.exports = Users;
