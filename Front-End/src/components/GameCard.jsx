139afcbee937bdcd7c9b0caeb77094ddf9f1c432
Adham <Adhammorsy75@gmail.com>
2023-12-06 18:03:23 -0500

    update

--- Front-End/src/components/GameCard.jsx
+++ Front-End/src/components/GameCard.jsx
@@ -7,4 +7,20 @@ const GameCard = ({ game, handleClick }) => {
     };
     return (
         <div className='game-card'>
-  
+            <img className='image' src={game.image} alt="Game's pic" />
+            <div className="name">
+                {game.name}
+            </div>
+            <div className='details'>
+                {game.details}
+            </div>
+            <div className="btn-container">
+                <button onClick={handleOpenModal} className="btn btn1">
+                    Add to Cart
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default GameCard;
