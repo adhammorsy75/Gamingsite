68be0fe4149d10c750b7d91d08992cc9d2f121b9
Adham <Adhammorsy75@gmail.com>
2023-12-06 19:29:34 -0500

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client.js'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
