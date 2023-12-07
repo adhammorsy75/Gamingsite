44c34784357d41bff371cf3e416ddcc929e0805a
Adham <Adhammorsy75@gmail.com>
2023-12-06 19:10:01 -0500

    commit

--- Front-End/src/components/Cart.jsx
+++ Front-End/src/components/Cart.jsx
@@ -10,6 +10,59 @@ const Cart = ({ setIsAuthenticated }) => {
     const [cart, setCart] = useState('');
 
     const fetchData = async () => {
-      
+        try {
+            const userID = user._id;
+            const res = await axios.get(`http://localhost:3002/cart/fetch/${userID}`);
+            if (res.status === 200) {
+                setCart(res.data);
+            }
+        } catch (error) {
+            if (error.response.status !== 404) {
+                window.alert('Internal Server Error');
+            }
+            else {
+                setCart('');
+            }
+        }
+    };
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const handleClearCart = async () => {
+        try {
+            const res = await axios.delete(`http://localhost:3002/cart/clear/${cart._id}`);
+            if (res.status === 200) {
+                fetchData();
+            }
+        } catch (error) {
+            window.alert('Internal Server Error');
+        }
+    };
+
+    return (
+        <div className='Cart'>
+            <NavBar setIsAuthenticated={setIsAuthenticated} />
+            <div className='cart-container'>
+                <h1>Your Cart</h1>
+                {cart ? (
+                    <div className='cart-items'>
+                        {cart.games?.map((item, index) => (
+                            <div key={index} className='cart-item'>
+                                <div className='item-details'>
+                                    <div className='game-name'>{item.game.name}</div>
+                                    <div className='quantity'>Quantity: {item.quantity}</div>
+                                </div>
+                            </div>
+                     
+                        </div>
+                    </div>
+                ) : (
+                    <div className='empty-cart'>Your cart is empty</div>
+                )}
+            </div>
+        </div>
+    );
+}
 
 export default Cart;
